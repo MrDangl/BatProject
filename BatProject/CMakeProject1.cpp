99a50344773c68040f4eabbc3bce7107be8f0ca5
MrDangl <glebdangl@gmail.com>
2023-12-18 03:49:35 +0800

    finished bat
    Lol it wasnt so hard but im stupid

--- BatProject/CMakeProject1.cpp
+++ BatProject/CMakeProject1.cpp
@@ -8,6 +8,7 @@
 #include <list>
 #include <map>
 #include <vector>
+#include <omp.h>
 #define MaxNvar  42
 using namespace std;
 
@@ -15,7 +16,7 @@ using namespace std;
 // using adjacency list representation
 class Edge {
     unsigned int start, end;
-    float probability = 0.8f;
+    double probability = 0.8;
     bool state;
     public:
     int getStart()
@@ -30,13 +31,21 @@ class Edge {
     {
         return state;
     }
+    double getProbability()
+    {
+        return probability;
+    }
     void changeState(bool change)
     {
         state = change;
     }
     Edge(int nodeStart, int nodeEnd)
     {
-        start = nodeStart; end = nodeEnd; state = false;
+        start = nodeStart; end = nodeEnd; state = true;
+    }
+    Edge(int nodeStart, int nodeEnd,bool statebool)
+    {
+        start = nodeStart; end = nodeEnd; state = statebool;
     }
 };
 
@@ -44,15 +53,18 @@ class Edge {
 class Graph {
 public:
     map<int, bool> visited;
-    map<int, list<int> > adj;
+    map<int, list<Edge*> > adj;
+    vector<int> nodes;
     vector<Edge*> edges;
 
     // Function to add an edge to graph
     void addEdge(int v, int w);
+    void addEdge(int v, int w,bool state);
+    double calculate_state();
 
     // DFS traversal of the vertices
     // reachable from v
-    void DFS(int v);
+    bool DFS(int v, int m);
     vector<Edge*> getEdges();
 };
 
@@ -60,23 +72,61 @@ void Graph::addEdge(int v, int w)
 {
     // Add w to v’s list.
     Edge *edge = new Edge(v, w);
-    adj[v].push_back(w);
+    adj[v].push_back(edge);
+    edges.push_back(edge);
+}
+void Graph::addEdge(int v, int w,bool state)
+{
+    // Add w to v’s list.
+    Edge* edge = new Edge(v, w,state);
+    adj[v].push_back(edge);
     edges.push_back(edge);
 }
 
-void Graph::DFS(int v)
+double Graph::calculate_state()
+{
+    double graphprobabil = 0;
+    
+    for (auto edge : edges)
+    {
+        if (edge->getState()) graphprobabil *= edge->getProbability();
+        else graphprobabil *= 1 - edge->getProbability();
+        if (graphprobabil == 0)
+        {
+            if (edge->getState()) graphprobabil = edge->getProbability();
+            else graphprobabil = 1 - edge->getProbability();
+        }
+    }
+    //std::cout << graphprobabil<<"\n";
+    return graphprobabil;
+}
+
+
+
+
+bool Graph::DFS(int v, int m)
 {
     // Mark the current node as visited and
     // print it
     visited[v] = true;
     cout << v << " ";
-
     // Recur for all the vertices adjacent
     // to this vertex
-    list<int>::iterator i;
-    for (i = adj[v].begin(); i != adj[v].end(); ++i)
-        if (!visited[*i])
-            DFS(*i);
+    list<Edge*>::iterator i;
+    for (auto i : adj[v]) {
+        if (!visited[i->getEnd()] && i->getState())
+        {
+            DFS(i->getEnd(), m);
+        }
+    }
+    //list<Edge>::iterator iter = std::find(adj[v].begin(), adj[v].end(), iter->getEnd()==m);
+    if (visited[m])
+    {
+        //cout << "found" << "\n";
+        return true;
+    }
+    else false;
+
 }
 
 vector<Edge*> Graph::getEdges()
@@ -97,8 +147,9 @@ int main()
     g.addEdge(3, 5);
     g.addEdge(4, 6);
     g.addEdge(4, 5);
-    g.addEdge(5, 7);
+    g.addEdge(5, 7,false);
     g.addEdge(6, 2);
+    g.addEdge(6, 0,false);
     g.addEdge(6, 8);
     g.addEdge(8, 9);
     g.addEdge(8, 10);
@@ -114,21 +165,27 @@ int main()
         std::cout << pointeredges[var]->getState()<<" ";
     }
     std::cout << "\n";
+    bool i = g.DFS(0,11);
+    std::cout << i;
+    double sum = 0;
     var = 0;
     do {
-        if (!pointeredges[var]->getState()) {
-            pointeredges[var]->changeState(true);
-            for (var = 0; var < pointeredges.size(); var++)
-            {
-                 
-                std::cout << " "<< pointeredges[var]->getState();
-                //sol++;
-            }
+        if (pointeredges[var]->getState()) {
+            pointeredges[var]->changeState(false);
+            #pragma omp parallel for reduction(+ : sum) 
+            //for (var = 0; var < pointeredges.size(); var++)
+            //{
+            //    std::cout << pointeredges[var]->getState() << " ";
+            //    
+            //    //sol++;
+            //}
+            if (g.DFS(0, 12)) sum += g.calculate_state();
+            std::cout << sum << "\n";
             var = 0;
             std::cout << "\n";
            // printf("%d", sol); printf("\n");
         }
-        else { pointeredges[var]->changeState(false); var++; }
+        else { pointeredges[var]->changeState(true); var++; }
     } while (var < pointeredges.size());
     printf("%d ",sol);
     return 0;

7daa2c81a142e696685221f254e8ba8af13b9b90
MrDangl <glebdangl@gmail.com>
2024-01-09 16:31:02 +0800

    new for branch

--- BatProject/CMakeProject1.cpp
+++ BatProject/CMakeProject1.cpp
@@ -156,24 +156,27 @@ int main()
         //example e
     g.addEdge(1, 2);
     g.addEdge(1, 3);
-    g.addEdge(2, 5);
-    g.addEdge(3, 7);
+    g.addEdge(2, 3);
+    g.addEdge(2, 4);
     g.addEdge(3, 4);
-    g.addEdge(4, 3);
-    g.addEdge(4, 5);
-    g.addEdge(5, 2);
-    g.addEdge(5, 6);
-    g.addEdge(5, 8);
-    g.addEdge(5, 4);
-    g.addEdge(6, 5);
-    g.addEdge(6, 7);
-    g.addEdge(7, 6);
-    g.addEdge(7, 3);
-    g.addEdge(7, 8);
-    g.addEdge(7, 9);
-    g.addEdge(8, 5);
-    g.addEdge(8, 7);
-    g.addEdge(8, 9);
+    //g.addEdge(2, 5);
+    //g.addEdge(3, 7);
+    //g.addEdge(3, 4);
+    //g.addEdge(4, 3);
+    //g.addEdge(4, 5);
+    //g.addEdge(5, 2);
+    //g.addEdge(5, 6);
+    //g.addEdge(5, 8);
+    //g.addEdge(5, 4);
+    //g.addEdge(6, 5);
+    //g.addEdge(6, 7);
+    //g.addEdge(7, 6);
+    //g.addEdge(7, 3);
+    //g.addEdge(7, 8);
+    //g.addEdge(7, 9);
+    //g.addEdge(8, 5);
+    //g.addEdge(8, 7);
+    //g.addEdge(8, 9);
 
 }
     unsigned int var, sol;
@@ -193,25 +196,66 @@ int main()
     //omp_set_num_threads(4);
     //cout << " max threads" << omp_get_max_threads();
 
+    //Test
+    int numberOfiter = powl(2, pointeredges.size());
+    std::cout << "number of supposed iter" << numberOfiter;
+    for (int i = 0; i < numberOfiter; i++)
+    {
+
+        sol++;
+        if (!pointeredges[var]->getState()) {
+            
+            pointeredges[var]->changeState(true);
+            var = 0;
+          // printf("%d", sol); printf("\n");
+        }
+        else { pointeredges[var]->changeState(false); var++; }
+        //Debug slow
+
+
+        //if (var > pointeredges.size() / 2) std::cout << "Half way there\n";
+        
+        for (int j = 0; j < pointeredges.size(); j++)
+        {
+            std::cout << pointeredges[j]->getState() << " ";
+            //sol++;
+        }
+        std::cout << sum << "\n";
+
+        g.clearVisited();
+
+        if (g.DFS(1, 4))
+        {
+            sum += g.calculate_state();
+        }
+        if (message) { cout << " number of threads" << omp_get_num_threads() << "\n"; message = false; }
+
+        
+
+    }
+    std::cout << " final score is " << sum <<"Itteration number "<< sol <<" How much should be " <<pow(2,pointeredges.size()) << "\n";
+
+    //
+
     //Идет перебор по состояниям,код выполняется если у нас не существует дуги
     // начинаем с пустого состояния всех дуг, потом потом по одной включаем 
     // и проверяем каждое состояние через DFS есть ли в данном состоянии указаный узел
     // если узел есть считаем состояние как произведение вероятностей для каждой дуги в схеме , где 1-вер для выключенной
     // 1+вер для включенной, потом мы суммируем все полученные вероятности состояний и получаем оценку для всей сети
-
+    /*
     do {
         if (!pointeredges[var]->getState()) {
             
             pointeredges[var]->changeState(true);
             
-            //Debug slow
+            ////Debug slow
             //for (var = 0; var < pointeredges.size(); var++)
             //{
             //    std::cout << pointeredges[var]->getState() << " ";
             //    
             //    //sol++;
             //}
-            //if (var > pointeredges.size() / 2) std::cout << "Half way there\n";
+            ////if (var > pointeredges.size() / 2) std::cout << "Half way there\n";
             //std::cout << sum << "\n";
 
             g.clearVisited();
@@ -229,6 +273,7 @@ int main()
         else { pointeredges[var]->changeState(false);var++; }
     } while (var < pointeredges.size());
     std::cout <<" final score is " << sum << "\n";
+   */ 
     return 0;
 }
 

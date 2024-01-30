b71894e3a19ce536164dc119d56be6840c647048
MrDangl <glebdangl@gmail.com>
2024-01-30 14:40:52 +0800

    Folder refactor

--- CMakeProject1.cpp
+++ CMakeProject1.cpp
@@ -344,13 +344,13 @@ double calculateGraphReliabilityFor(Graph n, string from, string to)
                 }
 
         //Debug slow
-        if (var > pointeredges.size() / 2) std::cout << "Half way there\n";
-        for (int j = 0; j < pointeredges.size(); j++)
-        {
-            std::cout << pointeredges[j]->getState() << " ";
-            //sol++;
-        }
-       std::cout << sum << "\n";
+        //if (var > pointeredges.size() / 2) std::cout << "Half way there\n";
+        //for (int j = 0; j < pointeredges.size(); j++)
+        //{
+        //    std::cout << pointeredges[j]->getState() << " ";
+        //    //sol++;
+        //}
+       //std::cout << sum << "\n";
 
 
                 Graph* temp = new Graph(n);
@@ -441,10 +441,12 @@ int main(int argc, char* argv[])
     std::cout << "Type path to graphml file \n";
     std::cin >> pathml;
     Graph e(pathml);
+
     std::cout << "\n Type probabil for edges \n";
     std::cin >> probabil;
     e.setExternalProbabil(probabil);
-    std::cout << "\n  + Type Begining node \n"<< probabil;
+
+    std::cout << "\n Type Begining node \n";
     std::cin >> start;
     std::cout << "\n Type Finish node \n";
     std::cin >> finish;
